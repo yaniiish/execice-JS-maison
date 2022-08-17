729ce0b30eb509b67fd93fea3828b59181753fa2
yaniiish <yanis.herda14@gmail.com>
2022-08-17 21:40:02 +0200

    exo maj

---
+++ exercice supplémentaires/ex7/script.js
@@ -0,0 +1,13 @@
+let nom = document.querySelector(".input-nom")
+let cin = document.querySelector(".input-cin")
+let mail = document.querySelector(".input-mail")
+let info = document.querySelector(".info")
+let envoyer = document.querySelector(".envoyer")
+
+mail.setCustomValidity("test !!!!!!")
+
+envoyer.addEventListener("click", verif)
+
+function verif (e) {
+
+}

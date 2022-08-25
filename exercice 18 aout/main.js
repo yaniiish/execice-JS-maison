26da3d052b142029e8c62e51ab46e95297ccc1e6
yaniiish <yanis.herda14@gmail.com>
2022-08-25 12:15:52 +0200

    fdbgfdbtr
    gjhj

--- exercice 18 aout/main.js
+++ exercice 18 aout/main.js
@@ -10,7 +10,8 @@ console.log(stagiaires[1].salutation());
 console.log(stagiaireC.nom);
 console.log(stagiaireD);
 
-
+let stagiaireA = new Stagiaire("martin", "vincent")
+console.log(stagiaireA);
 
 let dwwm = new Classe("DWWM", new Date(2022, 08, 20), new Date(2023, 03, 20))
 dwwm.ajouterStagiaire(stagiaireC)
@@ -19,15 +20,16 @@ dwwm.ajouterStagiaire(stagiaireD)
 console.log(dwwm);
 
 
-var stage;
-
+// _________________________________________
 
 var afficher = document.querySelector('.afficher');
 afficher.addEventListener('click', afficherStage);
+console.log(afficher);
 
 var creer = document.querySelector('.creer');
 creer.addEventListener('click', creerStage);
 
+var stage;
 var select;
 
 function creerStage() {
@@ -36,10 +38,9 @@ function creerStage() {
     var dateFin = document.querySelector(".dateFin")
     stage = new Classe(nomFormation.value, dateDebut.value, dateFin.value)
     console.log(nomFormation,dateDebut,dateFin);
-
+     
     select = document.createElement("select");
 
-    
     document.body.appendChild(ajoutStagiaireFormulaireNom);
     document.body.appendChild(ajoutStagiaireFormulairePrenom);
     document.body.appendChild(boutonAjoutStagiaire);
@@ -47,13 +48,12 @@ function creerStage() {
     
 }
 
+
 function afficherStage(){
     console.log(stage);
     var affichage = document.querySelector(".affichage");
-    affichage.innerHTML +=  `Le stage ${stage.intitule} commencera le ${stage.dateDebut} et finira le ${stage.dateFin}` ;
+    affichage.innerHTML +=  `La formation ${stage.intitule} commencera le ${stage.dateDebut} et finira le ${stage.dateFin}` ;
 } 
-
-
 
 var ajoutStagiaireFormulaireNom = document.createElement("input");
 ajoutStagiaireFormulaireNom.placeholder = "Nom stagiaire";
@@ -62,6 +62,7 @@ ajoutStagiaireFormulairePrenom.placeholder = "Prénom stagiaire";
 
 var boutonAjoutStagiaire = document.createElement("button");
 boutonAjoutStagiaire.innerText = "ajout stagiaire";
+
 boutonAjoutStagiaire.addEventListener("click", ajoutStagiaire);
 
 
